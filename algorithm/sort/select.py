579683c5a2f13f1d61867921bd2df2834d767690
LAPTOP-4H4MC690\e7217 <e7217@naver.com>
2021-08-25 23:08:07 +0900

    update algorithm

---
+++ algorithm/sort/select.py
@@ -0,0 +1,10 @@
+array = [1,2,6,3,4,8,9,7,5]
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i+1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+
+print(array)
